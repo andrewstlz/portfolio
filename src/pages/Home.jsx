2ecc9c5da53154b2b2efe4783f3e6df2bcc5a631
wongandrew1 <165810085+wongandrew1@users.noreply.github.com>
2025-11-10 00:15:19 -0500

    feat improved UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 export default function Home() {
   return (
     <section className="space-y-4">
-      <h1 className="text-3xl font-bold">
-        Hi! I am Andrew. Welcome to my portfolio.
-      </h1>
-      <p>I'm on a mission to build better brains.</p>
+      <h1 className="text-3xl font-dongle-bold">Welcome to my portfolio.</h1>
+      <p>
+        I'm on a mission to{" "}
+        <a className="underline decoration-[#37e3cf]">build better brains.</a>
+      </p>
     </section>
   );
 }
--- src/shared/RootLayout.jsx
+++ src/shared/RootLayout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout() {
       <header className="border-b border-white/10 sticky top-0 backdrop-blur">
         <div className="container flex items-center justify-between py-3">
           <NavLink to="/" className="text-lg font-semibold">
-            Andrew Wong
+            Andrew S. Wong
           </NavLink>
           <nav className="flex gap-1 text-sm">
             <LinkItem to="/" label="Home" />
@@ -34,11 +34,6 @@ export default function RootLayout() {
       <main className="container flex-1 py-8">
         <Outlet />
       </main>
-      <footer className="border-t border-white/10">
-        <div className="border-t border-white/10">
-          (C) {new Date().getFullYear()} Andrew's Portfolio
-        </div>
-      </footer>
     </div>
   );
 }
